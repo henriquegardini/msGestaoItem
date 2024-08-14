48798c6dc59fc43d90949b94cf2e3a445161e3bc
henriquegardini <henrique.gardini@hotmail.com>
2024-08-14 18:40:02 -0300

    primeiro commit

---
+++ src/main/java/techclallenge5/fiap/com/msGestaoItem/MsGestaoItemApplication.java
@@ -0,0 +1,13 @@
+package techclallenge5.fiap.com.msGestaoItem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestaoItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestaoItemApplication.class, args);
+	}
+
+}
