0f3a4c84f3a2fca784eb17998d57f2afc06791ff
unknown <alanpaullo@yahoo.com.br>
2024-09-11 19:17:28 -0300

    atualizacao

--- src/main/java/techclallenge5/fiap/com/msGestaoItem/feign/ProdutoClient.java
+++ src/main/java/techclallenge5/fiap/com/msGestaoItem/feign/ProdutoClient.java
@@ -3,6 +3,7 @@ package techclallenge5.fiap.com.msGestaoItem.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import reactor.core.publisher.Mono;
 import techclallenge5.fiap.com.msGestaoItem.model.Produto;
 
 @FeignClient(value = "produtoClient", url = "http://localhost:8086")
